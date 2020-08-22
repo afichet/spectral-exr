8ccb33c88e47901c47f2799e2d8d066077188082
Alban Fichet <alban.fichet@gmx.fr>
2020-08-23 00:41:10 +0200

    Write transmission and sensitivity spectra to EXR

--- app/merge-exr/main.cpp
+++ app/merge-exr/main.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <regex>
+#include <fstream>
 
 #include <OpenEXR/ImfRgbaFile.h>
 #include <OpenEXR/ImfArray.h>
@@ -22,6 +24,30 @@ bool checkExtension(const std::string& path) {
     }
 
     return true;
+}
+
+
+void load_csv(
+    const std::string& filename,
+    std::vector<float>& wavelengths_nm,
+    std::vector<float>& values)
+{
+    wavelengths_nm.clear();
+    values.clear();
+
+    const std::string floatRegex  = " *(\\d*\\.?\\d*([Ee][+-]?\\d+)?) *";
+    const std::regex e(floatRegex + "," + floatRegex);
+
+    std::ifstream inFile(filename);
+    std::string line;
+
+    while(std::getline(inFile, line)) {
+        std::smatch matches;
+        if (std::regex_search(line, matches, e)) {
+            wavelengths_nm.push_back(std::stof(matches[1]));
+            values.push_back(std::stof(matches[3]));
+        }
+    }
 }
 
 
@@ -63,9 +89,13 @@ int main(int argc, char* argv[]) {
 
     std::vector<float> spectralFramebuffer;
 
+    std::cout << "Using images:" << std::endl;
+
     for (size_t i = 0; i < files.size(); i++) {
         wavelengths.push_back(start_wl_nm + i * increment_wl_nm);
         Imf::RgbaInputFile file(files[i].c_str());
+        std::cout << "\t[At " << wavelengths[i] << "nm:] " << files[i] << std::endl;
+
         Imath::Box2i dw = file.dataWindow();
         const size_t c_width  = dw.max.x - dw.min.x + 1;
         const size_t c_height = dw.max.y - dw.min.y + 1;
@@ -94,7 +124,46 @@ int main(int argc, char* argv[]) {
 
     memcpy(&spectralImage(0, 0, 0), &spectralFramebuffer[0], width * height * wavelengths.size() * sizeof(float));
 
+    std::cout << std::endl;
+
+    if (argc >= 6) {
+        std::cout << "Using transmission / response spectrum information:" << std::endl;
+    }
+
+    if (argc >= 6) {
+        std::vector<float> camera_wavelengths_nm;
+        std::vector<float> camera_response;
+
+        std::cout << "\t[Camera response:] " << argv[5] << std::endl;
+
+        load_csv(argv[5], camera_wavelengths_nm, camera_response);
+        spectralImage.setCameraResponse(camera_wavelengths_nm, camera_response);
+    }
+
+    if (argc >= 7) {
+        std::vector<float> lens_wavelengths_nm;
+        std::vector<float> lens_transmission;
+
+        std::cout << "\t[Lens transmission:] " << argv[6] << std::endl;
+
+        load_csv(argv[6], lens_wavelengths_nm, lens_transmission);
+        spectralImage.setLensTransmission(lens_wavelengths_nm, lens_transmission);
+    }
+
+    if (argc >= 8) {
+        for (int c = 7; c < argc; c++) {
+            std::vector<float> filter_wavelengths_nm;
+            std::vector<float> filter_transmission;
+
+            std::cout << "\t[Filter transmission at " << wavelengths[c - 7] << "nm:] " << argv[6] << std::endl;
+
+            load_csv(argv[c], filter_wavelengths_nm, filter_transmission);
+            spectralImage.setChannelSensitivity(c - 7, filter_wavelengths_nm, filter_transmission);
+        }
+    }
+
     spectralImage.save(argv[4]);
+    std::cout << std::endl << "File saved as: " << argv[4] << std::endl;
 
     return 0;
 }
--- lib/include/EXRSpectralImage.h
+++ lib/include/EXRSpectralImage.h
@@ -33,6 +33,6 @@ class EXRSpectralImage : public SpectralImage {
         );
 
     static constexpr const char* SPECTRUM_TYPE_ATTR     = "Spectrum type"; 
-    static constexpr const char* LENS_TRANSMISSION_ATTR = "Lens transmission spectrum"; 
-    static constexpr const char* CAMERA_RESPONSE_ATTR   = "Lens transmission spectrum"; 
+    static constexpr const char* LENS_TRANSMISSION_ATTR = "Lens transmission"; 
+    static constexpr const char* CAMERA_RESPONSE_ATTR   = "Camera response"; 
 };
