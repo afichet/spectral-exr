3cbf21e3958dd4a054ba173b30d149f3598d06eb
Alban Fichet <alban.fichet@gmx.fr>
2021-05-27 16:47:07 +0200

    Format

--- lib/SpectrumAttribute.cpp
+++ lib/SpectrumAttribute.cpp
@@ -8,13 +8,13 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  *  * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *  * Redistributions in binary form must reproduce the above
  * copyright notice, this list of conditions and the following
  * disclaimer in the documentation and/or other materials provided
  * with the distribution.
- *
  *  * Neither the name of Institut d'Optique Graduate School, CNRS -
  * Universite de Bordeaux, Inria, Charles University nor the names of
  * its contributors may be used to endorse or promote products derived
@@ -33,7 +33,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 
 #include <SpectrumAttribute.h>
 
